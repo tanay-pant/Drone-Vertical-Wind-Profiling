a4a85ffb386ae58ba3658bc83c0ce33ce71ecdd0
Tanay Pant <panttanay1@gmail.com>
2025-12-27 16:17:16 -0600

    FIRST COMMIT

---
+++ weather_logger.py
@@ -0,0 +1,65 @@
+import requests
+import csv
+import time
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+API_KEY = os.getenv("WEATHER_API_KEY")  # sensitive information goes in .env
+LAT = os.getenv('pLAT')
+LON = os.getenv('pLONG')
+CITY = "CHICAGOLAND"
+
+OUTPUT_FILE = "ground_truth_wind.csv"
+
+def get_weather():
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        if response.status_code == 200: #client request received/processed by server
+            return {
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                "wind_speed_mph": data["wind"]["speed"],
+                "wind_deg": data["wind"]["deg"], #direction (0=North, 90=East)
+                "gust_mph": data["wind"].get("gust", "NA"), # gusts (if available, not for me where I am)
+                "humidity": data["main"]["humidity"],
+                "temp_f": data["main"]["temp"]
+            }
+        else:
+            print(f"Error: {data.get('message', 'Unknown error')}")
+            return None
+    except Exception as e:
+        print(f"Network Error: {e}")
+        return None
+
+def main():
+    print(f"--- Starting Weather Logger for {CITY} ---")
+    print(f"Saving to: {OUTPUT_FILE}")
+    print("Press CTRL+C to stop logging.\n")
+
+    # create csv with headers IF doesn't exist
+    try:
+        with open(OUTPUT_FILE, mode='x', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=["timestamp", "wind_speed_mph", "wind_deg", "gust_mph", "humidity", "temp_f"])
+            writer.writeheader()
+    except FileExistsError:
+        pass # file exists, append to it in the loop below
+
+    # constant writing to csv from API calls
+    while True:
+        weather_data = get_weather()
+        if weather_data:
+            with open(OUTPUT_FILE, mode='a', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=["timestamp", "wind_speed_mph", "wind_deg", "gust_mph", "humidity", "temp_f"])
+                writer.writerow(weather_data)
+            print(f"[{weather_data['timestamp']}] Wind: {weather_data['wind_speed_mph']} mph | Dir: {weather_data['wind_deg']}° | Humidity: {weather_data['humidity']}%")
+        # OpenWeatherMap updates an individual location's current weather data about every 10 minutes, so there's no point bombarding the csv with identical data
+        time.sleep(300)
+
+if __name__ == "__main__":
+    main()
